3de2f06cc1cc09e807a8a2687aaffd82aabd74c5
MarinuIvanova <marinulkagubina@gmail.com>
2025-10-23 20:34:41 +0700

    part one

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -1,6 +1,7 @@
 const users = require("../fixtures/users.json");
 const loginPage = require("../fixtures/pages/loginPage.json");
 const boxPage = require("../fixtures/pages/boxPage.json");
+const dashboardPage = require("../fixtures/pages/dashboardPage.json");
 const generalElements = require("../fixtures/pages/jeneral.json");
 import { faker } from "@faker-js/faker";
 
@@ -22,6 +23,10 @@ describe("user can create a box and run it", () => {
   //пользователь 1 запускает жеребьевку
 
   let newBoxName = faker.word.noun({ length: { min: 5, max: 10 } });
+  let boxname;
+  let minAmount = 10;
+  let maxAmount = 50;
+  let currency = "Евро";
 
   it("user logins and create a box", () => {
     cy.visit("/login");
@@ -32,6 +37,27 @@ describe("user can create a box and run it", () => {
     cy.contains("Создать коробку").should("exist");
     cy.contains("Создать коробку").click({ force: true });
     cy.get(boxPage.boxNameField).type(newBoxName);
+    cy.get(":nth-child(3) > .frm")
+      .invoke("val")
+      .then((response) => {
+        boxname = response;
+        cy.log(boxname);
+      });
     cy.get(generalElements.arrowRight).click();
+    cy.get(boxPage.sixthIcon).click();
+    cy.get(generalElements.arrowRight).click();
+    cy.get(boxPage.giftPriceToggle).check({ force: true });
+    cy.get(boxPage.minAmount).type(minAmount);
+    cy.get(boxPage.maxAmount).type(maxAmount);
+    cy.get(boxPage.currency).select(currency);
+    cy.get(generalElements.arrowRight).click();
+    cy.get(generalElements.arrowRight).click();
+    //cy.get(generalElements.arrowRight).click();
+    cy.get(dashboardPage.createdBoxName).should("have.text", newBoxName);
+    cy.get(".layout-1__header-wrapper-fixed .toggle-menu-item span")
+    .invoke('text')
+    .then((text)=>{
+      expect(text).to.include("УчастникиМоя карточкаПодопечный")
+    })
   });
 });
