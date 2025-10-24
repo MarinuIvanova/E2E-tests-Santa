997dd3096c939c46e48906fa73b58317ad49caa0
MarinuIvanova <marinulkagubina@gmail.com>
2025-10-25 00:08:52 +0700

    add config for cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
@@ -6,6 +11,12 @@ module.exports = defineConfig({
     testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+      on("file:preprocessor", bundler);
+      addCucumberPreprocessorPlugin(on, config);
+      return config;
     },
   },
 });
