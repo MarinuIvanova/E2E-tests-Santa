c81d9e430e90c82b7a352a4b38df03b44fb188f0
MarinuIvanova <marinulkagubina@gmail.com>
2025-10-25 18:38:48 +0700

    cucumber tests

--- cypress/e2e/test/login.feature
+++ cypress/e2e/test/login.feature
@@ -2,5 +2,14 @@ Feature: User login on Santa website
     Scenario: User logs in sucessfuly
 
         Given user is on santa login page
-        When user logs in
+        #When user logs in as "marinagubina37+1@gmail.com" and "test12345"
+        #When user logs in with table 
+        #    | login | password|
+        #   | marinagubina37+1@gmail.com | test12345 |
+        When user logs in as "<login>" and "<password>"
         Then user is on dashboard page
+        Examples:
+            | login | password | 
+            | marinagubina37+1@gmail.com  | test12345  | 
+            | marinagubina37+2@gmail.com  | BD7604  | 
+            | marinagubina37+3@gmail.com  | test12345  | 
--- cypress/support/step_definitions/login.cy.js
+++ cypress/support/step_definitions/login.cy.js
@@ -1,4 +1,5 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { da } from "@faker-js/faker";
 //import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 const users = require("../../fixtures/users.json");
 
@@ -6,8 +7,20 @@ Given("user is on santa login page", function () {
   cy.visit("/login");
 });
 
+/*When("user logs in as {string} and {string}", function (string, string2) {
+  cy.login(string, string2);
+});
+
 When("user logs in", function () {
   cy.login(users.userAutor.email, users.userAutor.password);
+});
+
+When("user logs in with table", function (dataTable) {
+  cy.login(dataTable.hashes()[0].login, dataTable.hashes()[0].password)
+});*/
+
+When("user logs in as {string} and {string}", function (login, password) {
+  cy.login(login, password);
 });
 
 Then("user is on dashboard page", function () {
