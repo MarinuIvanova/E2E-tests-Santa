44c159c7772ac130af335b5b529e68765c338768
MarinuIvanova <marinulkagubina@gmail.com>
2025-10-25 21:57:26 +0700

    add all tests cucumber

--- cypress/e2e/test/login.feature
+++ cypress/e2e/test/login.feature
@@ -2,14 +2,18 @@ Feature: User login on Santa website
     Scenario: User logs in sucessfuly
 
         Given user is on santa login page
-        #When user logs in as "marinagubina37+1@gmail.com" and "test12345"
+        When user logs in as "marinagubina37+1@gmail.com" and "test12345"
         #When user logs in with table 
         #    | login | password|
         #   | marinagubina37+1@gmail.com | test12345 |
-        When user logs in as "<login>" and "<password>"
+        #When user logs in as "<login>" and "<password>"
         Then user is on dashboard page
-        Examples:
+        #Examples:
             | login | password | 
             | marinagubina37+1@gmail.com  | test12345  | 
             | marinagubina37+2@gmail.com  | BD7604  | 
             | marinagubina37+3@gmail.com  | test12345  | 
+        
+        Then user enter name of box
+        Then get boxname
+        Then user choose icon
--- cypress/support/step_definitions/login.cy.js
+++ cypress/support/step_definitions/login.cy.js
@@ -1,7 +1,16 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-import { da } from "@faker-js/faker";
-//import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+import { faker } from "@faker-js/faker";
+
+const boxPage = require("../../fixtures/pages/boxPage.json");
 const users = require("../../fixtures/users.json");
+const dashboardPage = require("../../fixtures/pages/dashboardPage.json");
+const generalElements = require("../../fixtures/pages/jeneral.json");
+const invitePage = require("../../fixtures/pages/invitePage.json");
+const inviteeBoxPage = require("../../fixtures/pages/inviteeBoxPage.json");
+const inviteeDashboardPage = require("../../fixtures/pages/inviteeDashboardPage.json");
+
+let newBoxName = faker.word.noun({ length: { min: 5, max: 10 } });
+
 
 Given("user is on santa login page", function () {
   cy.visit("/login");
@@ -26,3 +35,23 @@ When("user logs in as {string} and {string}", function (login, password) {
 Then("user is on dashboard page", function () {
   cy.get(".toggle-menu-wrapper > a").click();
 });
+
+Then("user enter name of box", function () {
+  cy.get(boxPage.boxNameField).type(newBoxName);
+});
+
+Then("get boxname", function () {
+  cy.get(":nth-child(3) > .frm")
+    .invoke("val")
+    .then((response) => {
+      boxname = response;
+      cy.log(boxname);
+    });
+  cy.get(generalElements.arrowRight).click();
+});
+
+Then("user choose icon", function () {
+  cy.get(boxPage.sixthIcon).click();
+  cy.get(generalElements.arrowRight).click();
+});
+
